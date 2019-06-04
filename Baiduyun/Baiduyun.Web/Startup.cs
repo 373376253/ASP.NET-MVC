d017d5431a150154aa635493481d96b0d1a572d9
373376253 <373376253@users.noreply.github.com>
2019-06-04 09:50:18 +0800

    项目初始结构
    项目初始结构

---
+++ Baiduyun/Baiduyun.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Baiduyun.Web.Startup))]
+namespace Baiduyun.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
